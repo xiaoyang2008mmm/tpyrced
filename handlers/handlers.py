3ef793b3ef7170a81b9325688bd8e27612c0de64
root <root@JINAN-BACKUPS-SERVER.(none)>
2017-08-12 20:35:08 +0800

    对路由进行分类

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -9,45 +9,49 @@ import os.path
 STATIC_PATH   = os.path.join(os.path.dirname(__file__), "../static")
 TEMPLATE_PATH = os.path.join(os.path.dirname(__file__), "../templates")
 HANDLERS =[(r"/" ,				Index_Handler),
-	   (r"/iframe/(?P<page>\d*)" ,          Iframe_Handler),
-	   ##信息录入接口
+
+	######文员Url路由
+
 	   (r"/api/wenyuan/add/", 	WenYuanAdd_handler ),
 	   (r"/api/wenyuan/delete/", 	WenYuandelete_handler ),
-	   (r"/api/caiwu/delete/", 	Caiwudelete_handler ),
-	   (r"/api/biddingleft/delete/", 	biddingleftdelete_handler ),
-	   (r"/api/jingjia/add/",	JingJiaAdd_handler ),
-	   (r"/api/caiwuqianyue/add/", 	CaiWuAdd_handler ),
-	   (r"/api/caiwuticheng/add/", 	CaiWuAdd_handler ),
-
-
-	   #请求返回页面
-	   (r"/wenyuan/", 	wenyuan_handler ),
-	   (r"/customer_alter/(.*)", customer_alter_handler ),
-	   (r"/jingjia/", 	jingjia_handler ),
-	   (r"/caiwu/", 	caiwu_handler ),
+	   (r"/iframe/(?P<page>\d*)" ,          Iframe_Handler),
+	   (r"/wenyuan/", 		wenyuan_handler ),
+	   (r"/wenyuan/modify/",	wenyuan_modify_handler ),
+	   (r"/customer_alter/(.*)", 	customer_alter_handler ),
+       	   (r"/customerleft/(?P<page>\d*)",		customerleft_handler),
+	######财务URL路由
+	   (r"/caiwu/", 			caiwu_handler ),
 	   (r"/caiwu/add/", 	caiwu_add_handler ),
 	   (r"/caiwu/modify/", 	caiwu_modify_handler ),
-	   (r"/wenyuan/modify/",wenyuan_modify_handler ),
-	   #文员
- 
+	   (r"/api/caiwu/delete/", 	Caiwudelete_handler ),
+	   (r"/api/caiwuqianyue/add/", 	CaiWuAdd_handler ),
+	   (r"/api/caiwuticheng/add/", 	CaiWuAdd_handler ),
+           (r"/financeleft/(?P<page>\d*)",		financeleft_handler),
+	######竞价URL路由
+
+	   (r"/api/biddingleft/delete/", 	biddingleftdelete_handler ),
+	   (r"/api/jingjia/add/",	JingJiaAdd_handler ),
+	   (r"/jingjia/", 	jingjia_handler ),
+           (r"/biddingleft/(?P<page>\d*)",		biddingleft_handler),
+           (r"/biddingleft/add/",		biddingleft_add_handler),
+           (r"/biddingleft/modify/",	biddingleft_modify_handler),
 
 
+	######临时测试路由
 	   (r"/test/", 		test_handler ),
 		
-	    ###用户登录
+	###用户认证登录
            (r"/login/",                 Login_Handler),
            (r"/logout/",                Logout_Handler),
 
+	######用工管理路由
 
-       (r"/customerleft/(?P<page>\d*)",		customerleft_handler),
        (r"/employeeleft/",	employeeleft_handler),
+	######销售路由
        (r"/saleleft/",		saleleft_handler),
        (r"/sale/add/",		saleadd_handler),
        (r"/sale/modify/",		salemodify_handler),
-       (r"/financeleft/(?P<page>\d*)",		financeleft_handler),
-       (r"/biddingleft/(?P<page>\d*)",		biddingleft_handler),
-       (r"/biddingleft/add/",		biddingleft_add_handler),
-       (r"/biddingleft/modify/",	biddingleft_modify_handler),
+	#######系统设置路由
        (r"/systemleft/"  ,	systemleft_handler),
 
 
