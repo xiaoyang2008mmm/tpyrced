76d388693f42cf951180f2ea919442df9ada5e13
root <root@JINAN-BACKUPS-SERVER.(none)>
2017-08-13 22:36:45 +0800

    增加用户权限控制

--- views/auth.py
+++ views/auth.py
@@ -10,14 +10,21 @@ class Login_Handler(BaseHandler):
     def post(self):
         name = self.get_argument("login_username").encode("utf-8")
         password = self.get_argument("login_password").encode("utf-8")
-	if  name =="system":
-	    if password == "system":
-		self.set_cookie("anju_user", name)
-		self.write("ok")
+	try:
+	    get_user = TpyrcedUser.get(TpyrcedUser.user == name)
+	    if  get_user:
+	    	get_pwd = TpyrcedUser.get(TpyrcedUser.password == password)
+	        if get_pwd:
+		    url_index = (self.privilege_check(name)[0])[0]
+		    self.set_cookie("anju_user", name)
+		    self.write(url_index)
+	        else:
+	            self.write("密码不对!!!")
 	    else:
-	        self.write("密码不对!!!")
-	else:
-	    self.write("用户名不对!!!")
+	        self.write("用户名不对!!!")
+	except:
+	    self.write("没有此用户!!!")
+	
 
 class Logout_Handler(BaseHandler):
     """退出"""
